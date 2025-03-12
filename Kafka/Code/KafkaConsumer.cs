5bcd9bcccac15346a4d1c285daab46c4dd8ddb8e
Cadezzz <cader.reynaud@gmail.com>
2025-03-12 17:36:43 +0100

    Added KafkaConsumer.cs and KafkaProducer.cs and changed docker-compose.yml

---
+++ Kafka/Code/KafkaConsumer.cs
@@ -0,0 +1,26 @@
+﻿using Confluent.Kafka;
+namespace Kafka;
+
+public class KafkaConsumer
+{
+    public static void Main(string[] args)
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9092",
+            GroupId = "test-group",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+        {
+            consumer.Subscribe("test-topic");
+
+            while (true)
+            {
+                var consumeResult = consumer.Consume();
+                Console.WriteLine($"Received: {consumeResult.Message.Value}");
+            }
+        }
+    }
+}
---
+++ Kafka/Code/KafkaProducer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace Kafka // Update namespace based on your folder structure
+{
+    class KafkaProducer
+    {
+        public static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092" 
+            };
+
+            using var producer = new ProducerBuilder<string, string>(config).Build();
+
+            Console.WriteLine("Producing messages...");
+            
+            for (int i = 0; i < 5; i++)
+            {
+                var message = new Message<string, string>
+                {
+                    Key = i.ToString(),
+                    Value = "hello"
+                };
+
+                var result = await producer.ProduceAsync("test-topic", message);
+                Console.WriteLine($"Produced message: Key = {message.Key}, Value = {message.Value}, Partition = {result.Partition}");
+            }
+
+            producer.Flush(TimeSpan.FromSeconds(10));
+            Console.WriteLine("Done producing messages.");
+        }
+    }
+}
